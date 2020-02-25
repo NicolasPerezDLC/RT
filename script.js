f6dbed0a93375e1f36e8a44979ff1151b90a22fd
Perez de las Casas Nicolas <20265@columbushs.com>
2020-02-25 09:40:45 -0500

    a

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ resetButton.onclick = () =>{
     playScreen.style.visibility = "visible";
     gameScreen.style.visibility = "hidden";
 }
+
+// --------------------------------- timer --------------------------------- //
+
+let timerThree = document.getElementById('three')
+let timerTwo = document.getElementById('two')
+let timerOne = document.getElementById('one')
+
+let count = 3;
+function timer() {
+    if (count > 0) {
+        console.log(count);
+        count--;
+        setTimeout(timer, 700);
+    }
+    else {
+        timerThree.style.visibility = "hidden"
+        timerTwo.style.visibility = "visible"  
+    }
+}
+timer();
+
+let countTwo = 3;
+function timer2() {
+    if (countTwo > 0) {
+        console.log(countTwo);
+        countTwo--;
+        setTimeout(timer2, 1200);
+    }
+    else {
+        timerTwo.style.visibility = "hidden"
+        timerOne.style.visibility = "visible"  
+    }
+}
+timer2();
+
+let countThree = 3;
+function timer3() {
+    if (countThree > 0) {
+        console.log(countThree);
+        countThree--;
+        setTimeout(timer3, 1700);
+    }
+    else {
+        timerOne.style.visibility = "hidden"
+    }
+}
+timer3();
